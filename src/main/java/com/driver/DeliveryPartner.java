d2ec4fad724070dbd3621477b9cfffbcc5cab60b
Kunal Kasera <kunalkasera456@gmail.com>
2023-02-25 22:52:05 +0530

    solution_1

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -5,6 +5,7 @@ public class DeliveryPartner {
     private String id;
     private int numberOfOrders;
 
+
     public DeliveryPartner(String id) {
         this.id = id;
         this.numberOfOrders = 0;
